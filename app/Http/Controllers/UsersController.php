05710ee21e07e730df5513fd31e8de6532f68cd9
wongshuaibin <wongshuaibin@gmail.com>
2018-05-28 16:25:50 +0000

    Add user statuses

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,7 +34,10 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+                           ->orderBy('created_at', 'desc')
+                           ->paginate(30);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     public function store(Request $request)
@@ -116,4 +119,12 @@ class UsersController extends Controller
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function feed()
+    {
+       return $this->statuses()
+                   ->orderBy('created_at', 'desc');
+    }
+
+
 }
