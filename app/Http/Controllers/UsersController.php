8ff2ceca7d428ac77464cbc06860a2e41f7c6d8b
wongshuaibin <wongshuaibin@gmail.com>
2018-06-09 13:54:42 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -126,5 +126,19 @@ class UsersController extends Controller
                    ->orderBy('created_at', 'desc');
     }
 
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
 
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,13 +4,20 @@
 <div class="row">
   <div class="col-md-offset-2 col-md-8">
     <div class="col-md-12">
-        <div class="col-md-offset-2 col-md-8">
-            <section class="user_info">
-                @include('shared._user_info', ['user' => $user])
-            </section>
-        </div>
+      <div class="col-md-offset-2 col-md-8">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => $user])
+        </section>
+        <section class="stats">
+          @include('shared._stats', ['user' => $user])
+        </section>
+      </div>
     </div>
     <div class="col-md-12">
+      @if (Auth::check())
+        @include('users._follow_form')
+      @endif
+
       @if (count($statuses) > 0)
         <ol class="statuses">
           @foreach ($statuses as $status)
@@ -18,7 +25,7 @@
           @endforeach
         </ol>
         {!! $statuses->render() !!}
-       @endif
+      @endif
     </div>
   </div>
 </div>
